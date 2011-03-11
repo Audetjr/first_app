07f96933eed66d864715c974a83388a58e5ba2b0
Jack <jack@homeenergywatch.com>
2011-03-11 14:53:07 -0600

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'a2e585519cf7990f24c34dde71adfaa439c0dcd8e8a823568df0d82a2df1f74a25a48cab05d1450d10b44644a8bf44407e52b407a04c21abeeb6f591c04daa12'
